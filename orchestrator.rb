94105287710aaf3f86fa034f02dd844b9b06546c
Joakim Antman <joakim.antman@visma.com>
2016-03-30 09:38:49 +0300

    Renamed orchestrator

---
+++ orchestrator.rb
@@ -0,0 +1,44 @@
+#!/usr/bin/env ruby
+require 'rubygems'
+require 'daemons'
+require "bunny"
+require_relative 'consumers'
+
+puts '[ORCHESTRATOR] Starting..'
+
+class OrchestatorCreatedConsumer < Consumers::EventConsumer
+  def initialize
+    super('orchestrator.created', 'created')
+  end
+
+  def perform(delivery_info, metadata, payload)
+    puts "[ORCHESTRATOR] I got a new thing: #{payload.inspect}."
+    puts "[ORCHESTRATOR] Will pass it to generate transition.\n\n"
+
+    generate_queue.publish(payload, persistent: true)
+  end
+
+  def generate_queue
+    @generate_queue ||= create_queue_handle('transition.generate')
+  end
+end
+
+class OrchestatorErrorConsumer < Consumers::EventConsumer
+  def initialize
+    super('orchestrator.errors', 'error.#')
+  end
+
+  def perform(delivery_info, metadata, payload)
+    puts "[ORCHESTRATOR] Received an ERROR: #{payload.inspect}."
+    puts "[ORCHESTRATOR] I might be able to handle it, but will not.\n\n"
+  end
+end
+
+Daemons.run_proc('orchestrator', backtrace: true, dir: '.', log_output: true) do
+  OrchestatorCreatedConsumer.new.listen
+  OrchestatorErrorConsumer.new.listen
+
+  loop do
+    sleep(10)
+  end
+end
